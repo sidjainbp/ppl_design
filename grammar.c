14bafa6c60a1ffc63258ca08635711719096d849
akshayg03 <57269794+akshayg03@users.noreply.github.com>
2020-10-22 11:53:40 +0530

    Update grammar.c

--- grammar.c
+++ grammar.c
@@ -27,7 +27,7 @@ void grammar(FILE* ptr){
 	
     
     int i=0;
-	while(fgets(ptr, "%s%[^\n]", buf) != EOF){
+	while(fgets(buf, sizeof(buf), ptr ) != NULL){
 		lex = strtok(buf, " ");
 		arr[i].token = lex;
 
